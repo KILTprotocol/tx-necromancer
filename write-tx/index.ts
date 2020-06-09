3554bb141ad03d5deaf41fb8520443a3ee825757
Albrecht <14820950+weichweich@users.noreply.github.com>
2020-06-09 11:18:03 +0200

    update signatures

--- write-tx/index.ts
+++ write-tx/index.ts
@@ -8,8 +8,25 @@ const IN_FILE = "extrinsics.json"
 const NODE_ADDRESS = "ws://127.0.0.1:9945"
 const MNEMONIC = "//Alice"
 
+const SIG_TYPE_ED25519 = new Uint8Array([0]);
+
+function updateSig(signature) {
+  console.log("update to multisig")
+  return util.u8aConcat(
+    SIG_TYPE_ED25519,
+    signature
+  )
+}
+
+function mapArg(method, argNum, mapper) {
+  return (...args) => {
+    if (args.length >= argNum) args[argNum] = mapper(args[argNum])
+    return method(...args)
+  }
+}
+
 async function replayExtrinsics() {
-  console.log("Start")
+
   const wsProvider = new WsProvider(NODE_ADDRESS)
   const api = await ApiPromise.create({ provider: wsProvider, types: {
     DelegationNodeId: "Hash",
@@ -35,18 +52,19 @@ async function replayExtrinsics() {
     "CreateAttestation": api.tx.attestation.add,
     "RevokeClaim": api.tx.attestation.revoke,
     "CreateDelegationRoot": api.tx.delegation.createRoot,
-    "AddDelegation": api.tx.delegation.addDelegation,
+    "AddDelegation": mapArg(api.tx.delegation.addDelegation, 5, updateSig),
     "RevokeDelegation": api.tx.delegation.revokeDelegation,
   };
 
-  console.log("ready")
-  console.dir(api, {depth: 3})
+  console.log("May the finalized swim once again in the pool of living, to be compiled, authored and, once again, finalized.")
+  console.dir(CallIndices, {depth: 4})
 
   const extrinsics = JSON.parse(fs.readFileSync(IN_FILE, 'utf8'))
 
   let nonce = (await api.query.system.account(iamroot.address)).nonce
-
+  let counter = 0
   for (let oldTX of extrinsics) {
+    counter += 1
     console.log("Its a", oldTX.method)
 
     if (typeof CallIndices[oldTX.method] !== "undefined") {
@@ -57,6 +75,7 @@ async function replayExtrinsics() {
       await api.tx.sudo.sudoAs(oldTX.signer, tx).signAndSend(iamroot, {nonce})
     }
   }
+  console.log(`\n\nResurrected ${counter} transactions! May the dark node have mercy!`)
 }
 
 replayExtrinsics().then(() => {
